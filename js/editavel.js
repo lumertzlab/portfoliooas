3e9d9bf2242ef3539193aec3d9180f077174f6e0
Guilherme Alves Lumertz <glumertz@plataformaa.com.br>
2024-08-26 18:02:47 -0300

    cms and object pages added

---
+++ js/editavel.js
@@ -0,0 +1,54 @@
+const input = {
+  video_hero: "./img/video_hero.mp4",
+  tag_hero: "PORTFÓLIO",
+  titulo_hero: "Objetos de Aprendizagem",
+  texto_hero:
+    "Lorem ipsum dolor sit amet. Ea illum consequuntur est itaque galisum est accusamus facere rem optio voluptatem.",
+  texto_sec_objeto: "CONHEÇA NOSSOS OBJETOS DE APRENDIZAGEM",
+
+  objeto: [
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "Interativos H5P",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+      link_objeto: "../podcasts.html",
+    },
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "Interativos HTML",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "Objetos 3D",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "<i>Podcasts</i>",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "Vídeos",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+    {
+      img_objeto: "../img/bg-hero.png",
+      tag_objeto: "2 Subcategorias",
+      titulo_objeto: "Vídeos Imersivos",
+      texto_objeto:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+  ],
+};
