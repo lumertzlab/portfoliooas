a4e12104c1a333d52c7630419133efb0530cb2fb
Guilherme Alves Lumertz <glumertz@plataformaa.com.br>
2024-08-30 18:02:08 -0300

    object page w/ CMS

--- js/editavel.js
+++ js/editavel.js
@@ -14,7 +14,7 @@ const input = {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
     },
     {
-      img_objeto: "../img/bg-hero.png",
+      img_objeto: "../img/banner_html.png",
       titulo_objeto: "Interativos HTML",
       texto_objeto:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
@@ -26,13 +26,13 @@ const input = {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
     },
     {
-      img_objeto: "../img/bg-hero.png",
+      img_objeto: "../img/banner_podcast.png",
       titulo_objeto: "<i>Podcasts</i>",
       texto_objeto:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
     },
     {
-      img_objeto: "../img/bg-hero.png",
+      img_objeto: "../img/banner_video.png",
       titulo_objeto: "Vídeos",
       texto_objeto:
         "Confira a seguir alguns dos principais formatos do audiovisual em nosso catálogo.",
@@ -43,6 +43,110 @@ const input = {
       titulo_objeto: "Vídeos Imersivos",
       texto_objeto:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates officiis excepturi aut corrupti itaque.",
+    },
+  ],
+};
+
+const video = {
+  img_hero: "banner_video.png",
+  titulo_hero: "Vídeos",
+  texto_hero:
+    "Confira a seguir alguns dos principais formatos do audiovisual em nosso catálogo.",
+  item_menu: [
+    {
+      id: "sub01",
+      objeto: "Videoaula Expositiva",
+    },
+    {
+      id: "sub02",
+      objeto: "Vídeoaula Simulação",
+    },
+    {
+      id: "sub03",
+      objeto: "Vídeo Entrevista",
+    },
+  ],
+  item: [
+    {
+      id: "sub01",
+      objeto: "Videoaula Expositiva",
+      texto_objeto:
+        "Aulas em formato de vídeo que utilizam recursos visuais e auditivos para facilitar o aprendizado.",
+      video_samba: [
+        {
+          titulo_objeto: "A leitura para crianças de 0 a 3 anos",
+          area: "Educação",
+          ua: "38961",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/702b62a579ffe0ab1132b2e387769278",
+        },
+        {
+          titulo_objeto: "Diferentes técnicas de elicitação de requisitos",
+          area: "Exatas",
+          ua: "27558",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/db36a98bd42602f622e6f3dafc4e9320",
+        },
+        {
+          titulo_objeto: "Psicologia do esporte",
+          area: "Saúde",
+          ua: "52484",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/39d1b762d9ea2b93440a39a891b09b4b",
+        },
+        {
+          titulo_objeto: "Efeito chicote",
+          area: "Sociais aplicadas",
+          ua: "23190",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/5cab80ea55550628ab4c9d5ee911fc83",
+        },
+      ],
+    },
+    {
+      id: "sub02",
+      objeto: "Vídeoaula Simulação",
+      texto_objeto:
+        "Recria cenários de forma realista, permitindo que o estudante visualize situações do conteúdo de suas aulas.",
+      video_samba: [
+        {
+          titulo_objeto: "Entrevista inicial de Psicodiagnóstico",
+          area: "Saúde",
+          ua: "53227",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/7a0665af24583315ac0d3488a93d2a73",
+        },
+      ],
+    },
+    {
+      id: "sub03",
+      objeto: "Vídeo Entrevista",
+      texto_objeto:
+        "Gravação na qual um profissional compartilha suas perspectivas, experiências ou conhecimentos.",
+      video_samba: [
+        {
+          titulo_objeto:
+            "Práticas adaptadas para a inclusão na Educação Física",
+          area: "Educação",
+          ua: "18207",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/62b64721144bc6d347d601d6366ce9dd",
+        },
+        {
+          titulo_objeto: "Fatores humanos e Ergonomia",
+          area: "Exatas",
+          ua: "27049",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/e671d231bdf3ae3318bab8a9390bad7f",
+        },
+        {
+          titulo_objeto: "Direitos autorais",
+          area: "Sociais aplicadas",
+          ua: "14491",
+          link_objeto:
+            "https://fast.player.liquidplatform.com/pApiv2/embed/cee29914fad5b594d8f5918df1e801fd/e7a1dd209264175d090b29e1eba07d0e",
+        },
+      ],
     },
   ],
 };
